3f8631c1283d805b8fb719a8ef919b74982a72d1
张明瑞 <z1m6r3@gmail.com>
2015-02-18 10:18:49 +0800

    fix post func

--- js/bbs/bbs_UI.js
+++ js/bbs/bbs_UI.js
@@ -265,7 +265,7 @@ function UI_register_func_post_modal() {
     });
 
     $('#publish-post-button').click(UI_write_post);
-
+    $('#publish-post-button0').click(UI_write_post);
     $(document).on('click', '#write-post-panel .cancel-button', function() {
         if (confirm(bbs_string.confirm_cancel_post)) {
             UI_hide_write_post();
